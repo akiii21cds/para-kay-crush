e552c9d1d680dd0603e3390e41693b988ff5c673
Akiiiiiii <156560921+akiii21cds@users.noreply.github.com>
2024-02-13 21:11:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yun ohhh, chat kana para mag simula na yung love story natin! 😘 John chrisian Garete on fb";
+  question.innerHTML = .
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
